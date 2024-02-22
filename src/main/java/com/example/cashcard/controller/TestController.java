15e1c3cca7e4c149ddade7842f00618ebc8d3910
turkmuhendisi <1turkmuhendisi@gmail.com>
2024-02-22 16:31:42 +0300

    test driven implementing

---
+++ src/main/java/com/example/cashcard/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.example.cashcard.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+    @GetMapping("/hello")
+    public String getString() {
+        return "Selamun aleyküm Berat";
+    }
+}
