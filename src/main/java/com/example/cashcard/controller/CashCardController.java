c4d71629c2d63928a5d42201921dcdd7efa7b1f8
turkmuhendisi <1turkmuhendisi@gmail.com>
2024-03-08 20:10:57 +0300

    Implementing returning a list with GET

--- src/main/java/com/example/cashcard/controller/CashCardController.java
+++ src/main/java/com/example/cashcard/controller/CashCardController.java
@@ -2,11 +2,16 @@ package com.example.cashcard.controller;
 
 import com.example.cashcard.dao.CashCardRepository;
 import com.example.cashcard.model.CashCard;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -37,6 +42,17 @@ public class CashCardController {
                 .toUri();
 
         return ResponseEntity.created(locationOfNewCashCard).build();
+    }
+
+    @GetMapping
+    private ResponseEntity<List<CashCard>> findAll(Pageable pageable) {
+        Page<CashCard> page = cashCardRepository.findAll(
+                PageRequest.of(
+                        pageable.getPageNumber(),
+                        pageable.getPageSize(),
+                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
+                ));
+        return ResponseEntity.ok(page.getContent());
     }
 
 }
--- src/main/java/com/example/cashcard/dao/CashCardRepository.java
+++ src/main/java/com/example/cashcard/dao/CashCardRepository.java
@@ -2,5 +2,7 @@ package com.example.cashcard.dao;
 
 import com.example.cashcard.model.CashCard;
 import org.springframework.data.repository.CrudRepository;
-public interface CashCardRepository extends CrudRepository<CashCard, Long>{
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
 }
